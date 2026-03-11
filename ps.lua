77618ed5ce9eb627a3524fdd0efba5d9c84b4e3b
eIysia-dev <nnedyx19@gmail.com>
2026-03-11 09:36:24 +0800

    Create ps.lua

---
+++ ps.lua
@@ -0,0 +1,11 @@
+getgenv().PRIORITY   = {
+    "Meowl", "Strawberry elephant", "Skibidi toilet", "Headless horseman",
+    "Dragon Cannelloni", "Hydra dragon cannelloni", "Dragon Gingerini",
+    "Capitano moby", "La casa boo", "Fragrama and chocrama",
+    "La supreme combinasion", "Cooki and milki", "Burguro and fryuro",
+    "Garama and madungdung", "La secret combinasion", "Spooky and pumpky",
+    "Reinito sleighito", "Ketchuru and musturu", "La taco combinasion",
+    "Tralaledon", "Eviledon", "Los bros", "Tictac sahur", "Cerberus",
+    "Celestial Pegasus", "Love Love Bear", "Festive 67", "Ginger Gerat",
+    "Ketupat Bros", "Rosey and Teddy"
+}
