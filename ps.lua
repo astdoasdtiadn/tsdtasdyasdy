3fc47ceefe23dd2bb1e0d24a39f62f6927aa4e28
eIysia-dev <nnedyx19@gmail.com>
2026-03-18 21:32:13 +0800

    Update ps.lua

--- ps.lua
+++ ps.lua
@@ -3,7 +3,7 @@ getgenv().PRIORITY   = {
     "Dragon Cannelloni", "Hydra dragon cannelloni", "Dragon Gingerini",
     "Capitano moby", "La casa boo", "Fragrama and chocrama",
     "La supreme combinasion", "Cooki and milki", "Burguro and fryuro",
-    "Garama and madungdung", "La secret combinasion", "Spooky and pumpky",
+    "Garama and Madundung", "La secret combinasion", "Spooky and pumpky",
     "Reinito sleighito", "Ketchuru and musturu", "La taco combinasion",
     "Tralaledon", "Eviledon", "Los bros", "Tictac sahur", "Cerberus",
     "Celestial Pegasus", "Love Love Bear", "Festive 67", "Ginger Gerat",
