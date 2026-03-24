03d3fb1b695dd0998d7101d0bd44b4825a154f1d
eIysia-dev <nnedyx19@gmail.com>
2026-03-24 14:18:53 +0800

    Update ps.lua

--- ps.lua
+++ ps.lua
@@ -5,7 +5,7 @@ getgenv().PRIORITY   = {
     "La supreme combinasion", "Cooki and milki", "Burguro and fryuro",
     "Garama and Madundung", "La secret combinasion", "Spooky and pumpky",
     "Reinito sleighito", "La taco combinasion",
-    "Tralaledon", "Eviledon", "Los bros", "Tictac sahur", "Cerberus",
+    "Tralaledon", "Eviledon", "Los bros", "Cerberus",
     "Celestial Pegasus", "Love Love Bear", "Festive 67", "Ginger Gerat",
     "Ketupat Bros", "Rosey and Teddy", "Griffin", "Signore Carapace", "Dug Dug Dug",
     "Cloverat Clapat"
